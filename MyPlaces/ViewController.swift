3bc18be94a7639f9cf4d7e6131f207ae3f15575b
Dmitry Stekvashov <dimastekv@gmail.com>
2020-07-04 18:32:24 +0300

    Initial Commit

---
+++ MyPlaces/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyPlaces
+//
+//  Created by Dmitriy Stekvashov on 04.07.2020.
+//  Copyright © 2020 Dmitriy Stekvashov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
